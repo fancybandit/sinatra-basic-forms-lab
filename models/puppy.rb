da123e08dbe797ca69a6502f6a102d6a0536b26d
Jacob Klimenko <droidedjake@gmail.com>
2021-01-26 12:18:19 -0500

    commit tree 888299912958b0eca21531e95128b0413d6387f7

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,17 @@
-# since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+
+class Puppy
+    attr_accessor :name, :age
+    attr_reader :breed
+
+    def initialize(stats)
+        stats.each do |key, value|
+            if (key == :breed) || (key == "breed")
+                @breed = value
+            else
+                self.send(("#{key}="), value)
+            end
+        end
+    end
+
+end
+
